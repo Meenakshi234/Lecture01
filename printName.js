f14fe1b40c7e6afaaec377552ffd38185516d76c
Meenakshi234 <virk.meenakshi@gmail.com>
2021-11-10 12:46:53 -0500

    second cmd

--- printName.js
+++ printName.js
@@ -3,4 +3,5 @@ printNameFunc();
 function printNameFunc()
 {
     console.log("hello" + userName);
+    console.log("making second commit and tryine to find out why permission is denied while doing push command");
 }
